6c9b6b1c1aa623d1063de65e1f03362ea2e221e6
Kritika-Basnet2006 <bkritika035@gmail.com>
2025-07-11 18:27:22 +0545

    Second commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request,session,redirect,url_for,make_response,flash
+from flask import Flask, render_template,request,session,redirect,url_for,make_response,flash 
 
 app=Flask(__name__)
 app.secret_key="panda"
